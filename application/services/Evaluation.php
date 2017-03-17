f55d25090acb477f18e489cc51945ab70518b4e6
Bryan Richards <brichardssa@gmail.com>
2017-03-17 14:53:52 +0200

    Add corrective acction checklist to TB lab report

--- application/services/Evaluation.php
+++ application/services/Evaluation.php
@@ -1407,6 +1407,7 @@ class Application_Service_Evaluation {
                     $sampleStatuses);
                 $shipmentResult[$i]['eval_comment'] = $res['evaluationComments'];
                 $shipmentResult[$i]['optional_eval_comment'] = $res['optional_eval_comment'];
+                $shipmentResult[$i]['corrective_actions'] = $attributes['corrective_actions'];
                 $shipmentResult[$i]['responseResult'] = $toReturn;
             }
             $i++;
