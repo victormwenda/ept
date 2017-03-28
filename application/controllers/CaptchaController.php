bba26a01273808184f75f17c2489f9143213b1cf
Bryan Richards <brichardssa@gmail.com>
2017-03-28 11:25:53 +0200

    Neaten up forms with captcha's in them (trying to figure out how to produce unpredictable captcha)

--- application/controllers/CaptchaController.php
+++ application/controllers/CaptchaController.php
@@ -1,27 +1,19 @@
 <?php
 
-class CaptchaController extends Zend_Controller_Action
-{
-
-    public function init()
-    {
-        
+class CaptchaController extends Zend_Controller_Action {
+    public function init() {
         $ajaxContext = $this->_helper->getHelper('AjaxContext');
-        $ajaxContext->addActionContext('check-captcha', 'html')->initContext();        
-        
+        $ajaxContext->addActionContext('check-captcha', 'html')->initContext();
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         $this->_helper->layout()->disableLayout();
     }
 
-    public function checkCaptchaAction()
-    {
+    public function checkCaptchaAction() {
         if ($this->getRequest()->isPost()) {
             $params = $this->getRequest()->getPost();
             $session = new Zend_Session_Namespace('DACAPTCHA');
-            //$this->view->result = "success";
             if ($session->code == $params['challenge_field']) {
                  $this->view->result = "success";
             } else {
@@ -29,8 +21,6 @@ class CaptchaController extends Zend_Controller_Action
             }
         }
     }
-
-
 }
 
 
