e8a90d8e0b0def78e0b718d8053dc5ad7a5c4e19
Bryan Richards <brichardssa@gmail.com>
2021-01-26 13:45:39 +0200

    Add more space around Ultra performance graph legend items

--- cron/CronInit.php
+++ cron/CronInit.php
@@ -1,14 +1,14 @@
 <?php
- 
+
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
- 
+
 // Define application environment
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', 'development');
-    
-// Define CRON PATH 
+
+// Define CRON PATH
 defined('CRON_PATH')
     || define('CRON_PATH', realpath(dirname(__FILE__)));
 
@@ -24,13 +24,13 @@ set_include_path(implode(PATH_SEPARATOR, array(
 
 /** Zend_Application */
 require_once 'Zend/Application.php';
- 
+
 // Create application, bootstrap, and run
 $application = new Zend_Application(
     APPLICATION_ENV,
     APPLICATION_PATH . '/configs/application.ini'
 );
 
-Zend_Session::$_unitTestEnabled = true; 
+Zend_Session::$_unitTestEnabled = true;
 
 $application->bootstrap();
